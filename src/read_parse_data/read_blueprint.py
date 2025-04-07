d42bd20de2ee2f94aa5196e020b1023ca6804218
sumanthyahoo9 <sumanthyahoo9@gmail.com>
2025-04-07 11:11:44 +0100

    Additional functionality to handle building blueprints

--- src/read_parse_data/read_blueprint.py
+++ src/read_parse_data/read_blueprint.py
@@ -4,4 +4,6 @@ ready for OCR models.
 The facility layout is critical since it adds a lot of context beyond
 raw numbers for the LLM to make sense of the agent's actions.
 This script assumes that the blueprints are in the PDF format.
+Once we extract the blueprint using OCR, we can integrate it 
+with the epJSON file with the data about the zones
 """
